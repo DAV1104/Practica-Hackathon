9fbf9fd1585fca2fea069dc1fd70f98d5779d68a
DAV0411 <campomunozd7@gmail.com>
2024-04-29 18:35:58 -0500

    First Commit

---
+++ config/db.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:hola@localhost/hackathon'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.secretkey = '07492149d856d457df9a8baf19a625f7'
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
