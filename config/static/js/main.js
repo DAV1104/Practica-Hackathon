5694c0f8321523c254dee6cfc84b417a71403495
Jesús García <jesusd-garcial@unilibre.edu.co>
2024-04-29 21:54:08 -0500

    layout

---
+++ config/static/js/main.js
@@ -0,0 +1,18 @@
+function mostrarContenido() {
+    var contenido = document.getElementById("contenido_oculto");
+    if (contenido.style.display === "none") {
+        contenido.style.display = "block";
+    } else {
+        contenido.style.display = "none";
+    }
+}
+
+function mostrarContenido2() {
+    var contenido = document.getElementById("contenido_oculto2");
+    if (contenido.style.display === "none") {
+        contenido.style.display = "block";
+    } else {
+        contenido.style.display = "none";
+    }
+}
+
